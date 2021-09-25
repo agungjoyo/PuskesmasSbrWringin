5f611495bf75602cb71f7ddab05f98c867a52039
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2021-09-25 13:34:53 +0900

    .

--- projectpkm/lib/main.dart
+++ projectpkm/lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+//import firebase core plugins
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // The future is part of the state of our widget. We should not call `initializeApp`
+  // directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          //  return SomethingWentWrong();
+          return const Text('Database Not Connected !!!');
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return Loading();
+        return const Text('Loading...');
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
