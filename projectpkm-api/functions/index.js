f594cf0bb07d6b96d851cace37290fd0d645d9e0
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2021-09-29 21:01:58 +0700

    api user

--- projectpkm-api/functions/index.js
+++ projectpkm-api/functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
-const { createUser } = require('../controller/user');
 const { customAlphabet } = require("nanoid");
 
 const app = express();
@@ -41,4 +40,51 @@ app.post('/api/users/create', (req,res) => {
     })();
 });
 // end create user
+
+// get all user
+app.get('/api/users/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('Auth');
+            let response = [];
+            await query.get().then(querySnapshot => {
+            let docs = querySnapshot.docs;
+            for (let doc of docs) {
+                const selectedItem = {
+                    id: doc.id,
+                    Name: doc.data().Name,
+                    NIP:  doc.data().NIP,
+                    Address:  doc.data().Address,
+                    Position:  doc.data().Position,
+                    PhoneNumber:  doc.data().PhoneNumber,
+                    Password:  doc.data().Password
+                };
+                response.push(selectedItem);
+            }
+            });
+                return res.status(200).send(response);
+            } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// end get all user
+
+// get user by id
+app.get('/api/users/read/:userId', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.body.user_id);
+            let user = await document.get();
+            let response = user.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+// end get user by id
 exports.app = functions.https.onRequest(app);
