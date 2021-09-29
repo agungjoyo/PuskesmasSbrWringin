1eea146578de5a263d95596c6aa6df89b5934823
AnaFarida <anafarida1820@gmail.com>
2021-09-29 14:10:41 +0700

    Homepage masih proses
    Masih proses belum bisa dijalankan

--- projectpkm/lib/main.dart
+++ projectpkm/lib/main.dart
@@ -2,72 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:projectpkm/layouts.dart';
+import 'package:projectpkm/homepage.dart';
 //import firebase core plugins
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const App());
-}
-
-class App extends StatefulWidget {
-  const App({Key? key}) : super(key: key);
-  // Create the initialization Future outside of `build`:
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  // The future is part of the state of our widget. We should not call `initializeApp`
-  // directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          //  return SomethingWentWrong();
-          return const Text('Database Not Connected !!!');
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return const MyApp();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        // return Loading();
-        return const Text('Loading...');
-      },
-    );
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Puskesmas Sumber Wringin',
+      title: 'SAS-KIA Puskesmas Sumber Wringin',
+      debugShowCheckedModeBanner: false,
+      //locale: DevicePreview.locale(context), // Add the locale here
+      //builder: DevicePreview.appBuilder,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: GoogleFonts.sourceCodeProTextTheme(
-          Theme.of(context).textTheme
-        ).apply(
-          bodyColor: Colors.black
-        ),
-        pageTransitionsTheme: const PageTransitionsTheme(builders: {
-          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder()
-        }),
-        primaryColor: Colors.blue
-      ),
-      home: const SiteLayouts()
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          fontFamily: 'Barlow',
+          textTheme: TextTheme(
+              headline1: TextStyle(
+                  fontSize: 70,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              headline2: TextStyle(
+                  fontSize: 55,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              headline3: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontFamily: 'DMSerifDisplay'),
+              subtitle1: TextStyle(fontSize: 30, color: Colors.grey[500]),
+              subtitle2: TextStyle(fontSize: 20, color: Colors.grey[500]),
+              bodyText1:
+                  TextStyle(fontSize: 20, color: Colors.white, height: 1.25),
+              bodyText2:
+                  TextStyle(fontSize: 17, color: Colors.white, height: 1.25),
+              caption:
+                  TextStyle(fontSize: 15, color: Colors.white, height: 1.25),
+              button: TextStyle(fontSize: 17, color: Color(0xff1e1e24)))),
+      home: HomePage(),
     );
   }
 }
---
+++ projectpkm/lib/view/content_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:projectpkm/view/widgets/custom_tab.dart';
+
+class ContentView {
+  ContentView({@required this.tab, @required this.content});
+
+  final CustomTab tab;
+  final Widget content;
+}
