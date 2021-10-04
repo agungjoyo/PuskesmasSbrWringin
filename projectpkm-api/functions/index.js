f031350d336e4b22d41a0ba7a642c11b5ae95a94
Alvin-kor <55471978+Alvin-kor@users.noreply.github.com>
2021-10-04 09:14:14 +0700

    Update index.js
    Users Api Done

--- projectpkm-api/functions/index.js
+++ projectpkm-api/functions/index.js
@@ -2,7 +2,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
-const { customAlphabet } = require("nanoid");
 
 const app = express();
 
@@ -15,15 +14,13 @@ admin.initializeApp({
 const db = admin.firestore();
 
 app.use(cors({ origin: true }));
-// create user
-const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',20);
 
-const postId = nanoid();
-
+// User
+// Create User
 app.post('/api/users/create', (req,res) => {
     (async () => {
         try {
-            await db.collection('Auth').doc(postId)
+            await db.collection('Auth').doc(req.body.userId)
                 .create({
                     Name: req.body.name,
                     NIP: req.body.nip,
@@ -39,9 +36,9 @@ app.post('/api/users/create', (req,res) => {
         }
     })();
 });
-// end create user
+// End Create User
 
-// get all user
+// Get All User
 app.get('/api/users/read', (req, res) => {
     (async () => {
         try {
@@ -70,9 +67,9 @@ app.get('/api/users/read', (req, res) => {
         })();
     });
 
-// end get all user
+// End Get All User
 
-// get user by id
+// Get User By Id
 app.get('/api/users/read/search/id', (req, res) => {
     (async () => {
         try {
@@ -86,34 +83,22 @@ app.get('/api/users/read/search/id', (req, res) => {
         }
     })();
 });
-// end get user by id
+// End Get User By Id
 
-//get user by username
+
+//Get User By Username
 app.get('/api/users/read/search/name', (req, res) => {
     (async () => {
         try {
-            let query = db.collection('Auth');
+            let query = db.collection('Auth').where("Name", "==", req.body.name);
+            let searchUser = await query.get()
             let response = [];
-            response.slice(0,1);
-            await query.get().then(querySnapshot => {
-                let docs = querySnapshot.docs;
-                    for (let doc of docs) {
-                        const name = req.body.name;
-                        const nameInDatabase = doc.data().Name;
-                            if (name == nameInDatabase) {
-                                const searchName = { 
-                                id: doc.id,
-                                Name: doc.data().Name,
-                                NIP:  doc.data().NIP,
-                                Address:  doc.data().Address,
-                                Position:  doc.data().Position,
-                                PhoneNumber:  doc.data().PhoneNumber,
-                                Password:  doc.data().Password
-                                }
-                                response.push(searchName);
-                            }
-                    }
-            });
+            if (searchUser.empty) {
+                return res.status(200).send('No matching Users');
+              }  
+              searchUser.forEach(doc => {
+                response.push(doc.id, '=>', doc.data());
+              });
             return res.status(200).send(response);
         } catch (error) {
             console.log(error);
@@ -121,6 +106,45 @@ app.get('/api/users/read/search/name', (req, res) => {
         }
         })();
 });
-//end get user by usernaame
+//End Get User By Username
 
+// Update Users
+app.put('/api/users/update', (req, res) => {
+    (async() => {
+            try {
+                const userUpdate = db.collection('Auth').doc(req.body.userId);
+                await userUpdate.update({
+                    Name: req.body.name,
+                    NIP:  req.body.nip,
+                    Address:  req.body.address,
+                    Position:  req.body.position,
+                    PhoneNumber:  req.body.phoneNumber,
+                    Password:  req.body.password
+                })
+                return res.status(200).send('User Data Has Been Changed');
+            }
+            catch (error) {
+                console.log(error);
+                return res.status(500).send(error);
+            }
+    })();
+});
+// End Update Users
+
+// Delete Users
+app.delete('/api/users/delete', (req, res) => {
+    (async () => {
+        try {
+            const usersDelete = db.collection("Auth").doc(req.body.userId);
+            await usersDelete.delete();
+            return res.status(200).send('User Has Been Deleted !');
+        }
+        catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+// End Delete Users
+// End User
 exports.app = functions.https.onRequest(app);
