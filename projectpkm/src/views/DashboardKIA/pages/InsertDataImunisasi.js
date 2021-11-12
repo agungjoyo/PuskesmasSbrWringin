e6229b79f40ec093965d28d2985a31bf04720dfe
Alvin <55471978+Alvin-kor@users.noreply.github.com>
2021-11-12 11:30:45 +0900

    array

--- projectpkm/src/views/DashboardKIA/pages/InsertDataImunisasi.js
+++ projectpkm/src/views/DashboardKIA/pages/InsertDataImunisasi.js
@@ -1,86 +1,47 @@
 import React, { Component } from "react";
 //material
-import { Box, Grid, Container, Typography, Card } from "@mui/material";
+import { Box, Grid, Container, Typography, Card, Fab } from "@mui/material";
 // import TableBody from "@mui/material/TableBody";
 import * as XLSX from "xlsx";
+import AddIcon from "@material-ui/icons/Add";
 import Page from "../components/Page";
 
 //import { initial } from "lodash";
 
 class InsertDataImunisasi extends Component {
-<<<<<<< HEAD
+  state = {
+    files: [],
+  };
+  readExcell = (event) => {
+    this.setState({ files: event });
+    const file = event.target.files[0];
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsBinaryString(file);
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "binary" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+        resolve(data);
+        const tahunSet = data[3][2];
+        const tahunSplit = tahunSet.split(" ");
+        const tahun = tahunSplit[1];
+        console.log(tahun);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d);
+    });
+  };
   render() {
-    // const [data, setdata] = useState(initialState);
-
-    const readExcell = (file) => {
-      const promise = new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.readAsArrayBuffer(file);
-
-        fileReader.onload = (e) => {
-          const bufferArray = e.target.result;
-
-          const wb = XLSX.read(bufferArray, { type: "buffer" });
-
-          const wsname = wb.SheetNames[0];
-=======
-  constructor() {
-    super();
-    this.state = {
-      files: [],
-      Tahun: "",
-      Bulan: "",
-      Puskesmas: "",
-      Sasaran: "",
-      HBOLess1day: "", //HBo<24jam
-      HBOOneWeek: "", //HBO0-7hari
-      BCG: "",
-      Polio1: "",
-      DPTHB1: "", //DPT-HB1
-      Polio2: "",
-      DPTHB2: "",
-      Polio3: "",
-      DPTHB3: "",
-      Polio4: "",
-      IPV: "",
-      CampakRubella: "",
-      IDL: "",
-    };
-  }
-  onDrop(files) {
-    this.setState({ files });
-    var file = files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-      csv.parse({ delimiter: ";" }, reader.result, (err, data) => {
-        const tahun = data[3][2];
-        const bulan = data[4][2];
-        const tahunSplit = tahun.split(" ");
-        const bulanSplit = bulan.split(" ");
-        console.log(tahunSplit, bulanSplit);
-        this.setState({
-          Tahun: tahunSplit[1],
-          Bulan: bulanSplit[1],
-        });
->>>>>>> cc9df7b97958bc8b5baacf267f8f0a9ff3573c66
-
-          const ws = wb.Sheets[wsname];
-
-          const data = XLSX.utils.sheet_to_json(ws);
-
-          resolve(data);
-        };
-
-        fileReader.onerror = (error) => {
-          reject(error);
-        };
-      });
-
-      promise.then((d) => {
-        console.log(d);
-      });
-    };
-
+    console.log(this.state);
     return (
       <Page title="Dashboard | Imunisasi">
         <Container maxWidth="xl">
@@ -106,13 +67,28 @@ class InsertDataImunisasi extends Component {
                 }}
               >
                 <div>
-                  <input
-                    type="file"
-                    onChange={(e) => {
-                      const file = e.target.files[0];
-                      readExcell(file);
-                    }}
-                  ></input>
+                  <label htmlFor="uploadImun">
+                    <input
+                      style={{ display: "none" }}
+                      type="file"
+                      id="uploadImun"
+                      name="uploadImun"
+                      onChange={this.readExcell.bind(this)}
+                      //   (e) => {
+                      //   const file = e.target.files[0];
+                      //   readExcell(file);
+                      // }}
+                    />
+                    <Fab
+                      color="secondary"
+                      size="medium"
+                      component="span"
+                      aria-label="add"
+                      variant="extended"
+                    >
+                      <AddIcon /> Upload File
+                    </Fab>
+                  </label>
                   <p style={{ margin: 30 }}>
                     upload file Anda disini atau klik pilih file
                   </p>
